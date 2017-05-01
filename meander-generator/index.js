cb922e8e81c402f5fd92100f7e7e08933309e5e6
vova <volodymyrtymets@gmail.com>
2017-05-01 14:31:45 +0300

    add more test code

--- meander-generator/index.js
+++ meander-generator/index.js
@@ -1,11 +1,13 @@
 const Gpio = require('onoff').Gpio;
 const button = new Gpio(1, 'in');
-const out = new Gpio(1, 'out');
+// const out = new Gpio(1, 'out');
 
 console.log('Start');
 
-button.watch(function(err, value) {
-  console.log(`value of 1 input -> ${value}`)
-});
-out.writeSync(1);
+// button.watch(function(err, value) {
+//   console.log(`value of 1 input -> ${value}`)
+// });
+button.writeSync(1);
+const value = button.readSync();
+console.log(`value of 1 input -> ${value}`)
 console.log('end');
