7638de63de0c9490fbf0661804672471c7a9ff44
vova <volodymyrtymets@gmail.com>
2017-05-01 14:21:01 +0300

    add test code

--- meander-generator/index.js
+++ meander-generator/index.js
@@ -1,1 +1,9 @@
-console.log('helow its meander  henarator');
+const Gpio = require('onoff').Gpio;
+const button = new Gpio(1, 'in', 'both');
+
+console.log('Start');
+
+button.watch(function(err, value) {
+  console.log(`value of 1 input -> ${value}`)
+});
+console.log('end');
