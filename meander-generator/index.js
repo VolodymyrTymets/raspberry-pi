ae9fc19f19010a247322308ebf9fcdc691990155
vova <volodymyrtymets@gmail.com>
2017-05-01 14:42:57 +0300

    add more test code

--- meander-generator/index.js
+++ meander-generator/index.js
@@ -1,13 +1,28 @@
 const Gpio = require('onoff').Gpio;
-const button = new Gpio(1, 'in');
+const led = new Gpio(14, 'out'),
 // const out = new Gpio(1, 'out');
 
-console.log('Start');
+console.log('----------------------> Start')
 
 // button.watch(function(err, value) {
 //   console.log(`value of 1 input -> ${value}`)
 // });
-button.writeSync(1);
-const value = button.readSync();
-console.log(`value of 1 input -> ${value}`)
-console.log('end');
+
+// Toggle the state of the LED on GPIO #14 every 200ms.
+// Here synchronous methods are used. Asynchronous methods are also available.
+const iv = setInterval(function () {
+  led.writeSync(led.readSync() ^ 1); // 1 = on, 0 = off :)
+}, 200);
+
+const ivlog = setInterval(function () {
+  const value = led.readSync();
+  console.log(`value of 14 input -> ${value}`);
+}, 50);
+// Stop blinking the LED and turn it off after 5 seconds.
+setTimeout(function () {
+  clearInterval(iv); // Stop blinking
+  clearInterval(ivlog);
+  led.writeSync(0);  // Turn LED off.
+  led.unexport();    // Unexport GPIO and free resources
+  console.log('----------------------> End')
+}, 5000);
