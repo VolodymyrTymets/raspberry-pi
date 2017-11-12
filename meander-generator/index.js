a55e676c3280e640e7e2611ce87a8e2944ad00b6
vova <volodymyrtymets@gmail.com>
2017-11-12 13:08:18 +0200

    little fix

--- meander-generator/index.js
+++ meander-generator/index.js
@@ -1,7 +1,7 @@
 const generateMeanderFor = require('./generator');
 
-const time =  process.argv[3] || 500;
-const gpio =  parseInt(process.argv[4]) || 21;
+const time =  process.argv[2] || 500;
+const gpio =  parseInt(process.argv[3]) || 21;
 
 console.log(`Start for ${gpio} with interval ${time}`)
 generateMeanderFor(gpio, time); // for 14 BCM pinm each 20 ms
