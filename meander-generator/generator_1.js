4e6c6672f2f99874e0cc2171637758cf9799f192
vova <volodymyrtymets@gmail.com>
2018-01-29 18:01:59 +0200

    add gpio generato 1

--- meander-generator/generator_1.js
+++ meander-generator/generator_1.js
@@ -1,19 +1,16 @@
-//const Gpio = require('onoff').Gpio;
+const Gpio = require('onoff').Gpio;
 
 const generateMeanderFor = (number, time) => {
   let step = false;
-  //const out = new Gpio(number, 'out');
+  const out = new Gpio(number, 'out');
   setInterval(function () {
     step = !step;
     if (step) {
-      let previous = process.hrtime();
       let stepPrevious = process.hrtime();
       do {
         const now = process.hrtime();
-        const diff = process.hrtime(previous);
         const stepDif = process.hrtime(stepPrevious);
-        console.log('-->', diff[1] / 1000)
-        previous = now;
+        out.writeSync(out.readSync() ^ 1);
         if (stepDif[1] / (1000 * 1000) > time) {
           stepPrevious = now;
           break;
