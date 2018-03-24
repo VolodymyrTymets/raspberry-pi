7844507abec8eb1d197b3c531c1b0976d9f104ad
vova <volodymyrtymets@gmail.com>
2018-03-24 10:02:20 +0200

    add for 2 gpio

--- rectnmgle-generator/for-1.js
+++ rectnmgle-generator/for-1.js
@@ -1,17 +1,18 @@
 const sleep = require('sleep');
 const Gpio = require('onoff').Gpio;
 
-const frequently =  1000000 / (process.argv[2] || 1);
-const timeONinPersent =  process.argv[3] || 20;
-const gpioNumber =  parseInt(process.argv[4]) || 21;
+const freqArg = process.argv[2] || 1;
+const frequently =  1000000 / freqArg;
+const timeONinPersent =  process.argv[3] || 50;
+const gpioNumber =  parseInt(process.argv[4]) || 14;
 
 let isOn = true;
 let out = null;
 try {
+  console.log(`run on gpio [${gpioNumber}] with frequently ${freqArg} Hz and ${timeONinPersent}% on.`);
   out = new Gpio(gpioNumber, 'out');
-  console.log('run on gpio ->', gpioNumber);
-} catch (err) {
-  console.log('GPIO is not detected!!!');
+ } catch (err) {
+  console.log('Error -> GPIO is not detected!!!');
 }
 
 do {
---
+++ rectnmgle-generator/for-2.js
@@ -0,0 +1,33 @@
+const sleep = require('sleep');
+const Gpio = require('onoff').Gpio;
+
+const freqArg = process.argv[2] || 1;
+const frequently =  1000000 / freqArg;
+const timeONinPersent =  process.argv[3] || 50;
+const gpioNumber1 =  parseInt(process.argv[4]) || 14;
+const gpioNumber2 =  parseInt(process.argv[5]) || 15;
+
+let isOn = true;
+let out1 = null;
+let out2 = null
+try {
+  console.log(`run on gpio [${gpioNumber1}] [${gpioNumber2}] with frequently${freqArg} Hz and ${timeONinPersent}% on.`);
+  out1 = new Gpio(gpioNumber1, 'out');
+  out2 = new Gpio(gpioNumber2, 'out');
+} catch (err) {
+  console.log('GPIO is not detected!!!');
+}
+
+do {
+  const toSleep = !isOn ?
+    frequently * (timeONinPersent / 100) :
+    frequently - (frequently * (timeONinPersent / 100));
+  isOn = !isOn;
+  if (out1) {
+    out1.writeSync(isOn ? 1: 0);
+    out2.writeSync(isOn ? 0: 1);
+  } else {
+    console.log(isOn ? 'on': 'of');
+  }
+  sleep.usleep(toSleep);
+} while (true);
