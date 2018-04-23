77b629fbf47f7b67d0541daffbd65b4d782b7b47
vova <volodymyrtymets@gmail.com>
2018-04-23 20:27:13 +0300

    code change

--- rectnmgle-generator/index_1.js
+++ rectnmgle-generator/index_1.js
@@ -1,9 +1,23 @@
 const _ = require('lodash');
 const sleep = require('sleep');
+const Gpio = require('onoff').Gpio;
 
-const freqArg = process.argv[2] || 1;
+let out1 = null;
+let out2 = null;
+const outNumber1  = process.argv[4] && parseInt(process.argv[4]) || 14;
+const outNumber2  = process.argv[4] && parseInt(process.argv[5]) || 15;
+const freqArg = process.argv[2] && parseInt(process.argv[2]) || 1;
 const position = process.argv[3] || 1;
 const stepTime = _.round((1000000 / freqArg) / 6);
+
+try {
+  console.log(`run on gpio [${outNumber1}] [${outNumber2}] with frequency [${freqArg}] and position [${position}]`);
+  out1 = new Gpio(14, 'out');
+  out2 = new Gpio(15, 'out');
+} catch (err) {
+  console.log('Error -> GPIO is not detected!!!');
+  process.exit();
+}
 
 console.log(stepTime, position);
 
@@ -17,18 +31,20 @@ let step = 0;
 const times = positionMap[position] || positionMap['1'];
 console.log('times ->', times)
 
-let str = '_';
-
 do {
-  if(step === 5) {
+  if(step === 6) {
     step = 0;
-    str = str + '  ';
+    out2.writeSync(0);
   }
-  if(step === 1) {
-    str = str + '-'
+  if(step === 2) {
+    out1.writeSync(1)
   }
-  str = str + '_';
-  process.stdout.write(str);
-  sleep.usleep(times[step]);
+  if(step === 3) {
+    out1.writeSync(0)
+  }
+  if(step === 5) {
+    out2.writeSync(1)
+  }
+  sleep.usleep(times[step] - 1);
   step ++;
 } while (true);
